2967852607ad111d921e2a4236093916c7f79603
Nuru1818 <nu.ru@yahoo.com>
2024-04-30 21:27:38 +0700

    Update Doctor Schedule Controller

--- app/Http/Controllers/DoctorScheduleController.php
+++ app/Http/Controllers/DoctorScheduleController.php
@@ -33,34 +33,85 @@ class DoctorScheduleController extends Controller
     {
         $request->validate([
             'doctor_id' => 'required',
-            'day' => 'required',
-            'time' => 'required',
         ]);
 
-        $doctorSchedule = new DoctorSchedule;
-        $doctorSchedule->doctor_id = $request->doctor_id;
-        $doctorSchedule->day = $request->day;
-        $doctorSchedule->time = $request->time;
-        $doctorSchedule->status = $request->status;
-        $doctorSchedule->note = $request->note;
-        $doctorSchedule->save();
+        // if senin is not empety
+        if($request->senin){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Senin';
+            $doctorSchedule->time = $request->senin;
+            $doctorSchedule->save();
+        }
 
-        return redirect()->route('doctor-schedules.index');
+        // if selasa is not empety
+        if($request->selasa){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Selasa';
+            $doctorSchedule->time = $request->selasa;
+            $doctorSchedule->save();
+        }
+
+        // if rabu is not empety
+        if($request->rabu){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Rabu';
+            $doctorSchedule->time = $request->rabu;
+            $doctorSchedule->save();
+        }
+
+        // if kamis is not empety
+        if($request->kamis){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Kamis';
+            $doctorSchedule->time = $request->kamis;
+            $doctorSchedule->save();
+        }
+
+        // if jumat is not empety
+        if($request->jumat){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Jumat';
+            $doctorSchedule->time = $request->jumat;
+            $doctorSchedule->save();
+        }
+
+        // if sabtu is not empety
+        if($request->sabtu){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Sabtu';
+            $doctorSchedule->time = $request->sabtu;
+            $doctorSchedule->save();
+        }
+
+        // if minggu is not empety
+        if($request->minggu){
+            $doctorSchedule = new DoctorSchedule;
+            $doctorSchedule->doctor_id = $request->doctor_id;
+            $doctorSchedule->day ='Minggu';
+            $doctorSchedule->time = $request->minggu;
+            $doctorSchedule->save();
+        }
+
+        return redirect()->route('doctor-schedules.index')->with('success', 'Data berhasil ditambahkan');
     }
 
     public function edit($id)
     {
-        $doctorSchedule = DoctorSchedule::find($id);
+        $doctorSchedules = DoctorSchedule::find($id);
         $doctors = Doctor::all();
-        return view('pages.doctor_schedules.edit', compact('doctorSchedule', 'doctor'));
+        return view('pages.doctor_schedules.edit', compact('doctorSchedules', 'doctors'));
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
             'doctor_id' => 'required',
-            'day' => 'required',
-            'time' => 'required',
         ]);
 
         $doctorSchedule = DoctorSchedule::find($id);
@@ -76,8 +127,8 @@ class DoctorScheduleController extends Controller
 
     public function destroy($id)
     {
-        $doctorSchedule = find($id)->delete();
-
-        return redirect()->route('doctor-schedules.index');
+        $doctorSchedule = DoctorSchedule::find($id);
+        $doctorSchedule->delete();
+        return redirect()->route('doctor-schedules.index')->with('success','Doctor Schedules deleted successfully.');
     }
 }
